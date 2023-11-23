670350d2217b751794b9ff59a1518f3f9f738572
mathslimin <mathslimin@qq.com>
2023-11-23 22:01:18 +0800

    Update search-a-2d-matrix.c

--- array/search-a-2d-matrix.c
+++ array/search-a-2d-matrix.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) {
     int start,end;
     int mid;
@@ -18,4 +25,14 @@ bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target
     }
 
     return false;
+}
+
+int main() {
+    int matrix[3][4] = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+    int* matrix_ptr[3] = {matrix[0], matrix[1], matrix[2]};
+    int matrixColSize[3] = {4, 4, 4};
+    int target = 3;
+    bool result = searchMatrix(matrix_ptr, 3, matrixColSize, target);
+    printf("The result is %s", result ? "true" : "false");
+    return 0;
 }
