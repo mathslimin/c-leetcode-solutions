eff56b6f88209ccf3a70775bc91d8f342a033011
mathslimin <mathslimin@qq.com>
2023-11-23 21:35:39 +0800

    Update best-time-to-buy-and-sell-stock[2].c

--- array/best-time-to-buy-and-sell-stock[2].c
+++ array/best-time-to-buy-and-sell-stock[2].c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 int maxProfit(int* prices, int pricesSize) {
     int min = INT_MAX;
     int i;
@@ -9,4 +16,14 @@ int maxProfit(int* prices, int pricesSize) {
     }
 
     return ret;
+}
+
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int pricesSize = sizeof(prices) / sizeof(prices[0]);
+    
+    int result = maxProfit(prices, pricesSize);
+    printf("Max profit: %d\n", result);
+    
+    return 0;
 }
