343545b55d4c4316c8fd8a861e2f3b2039fb212e
mathslimin <mathslimin@qq.com>
2023-11-23 21:48:26 +0800

    Update jump-game.c

--- array/jump-game.c
+++ array/jump-game.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 bool canJump(int* nums, int numsSize) {
     int i = 0;
     int max_jump = nums[0];
@@ -13,4 +20,12 @@ bool canJump(int* nums, int numsSize) {
     }
 
     return true;
+}
+
+int main() {
+    int nums[] = {2, 3, 1, 1, 4};
+    int numsSize = sizeof(nums) / sizeof(int);
+    int steps = canJump(nums, numsSize);
+    printf("Minimum number of jumps to reach the end: %d", steps);
+    return 0;
 }
