5a8f7d93a2effeb99e290e1bc680541e8981a20a
mathslimin <mathslimin@qq.com>
2023-11-23 21:52:17 +0800

    Update merge-sorted-array.c

--- array/merge-sorted-array.c
+++ array/merge-sorted-array.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 void merge(int* nums1, int m, int* nums2, int n) {
     int end = m + n - 1;
     int i = m - 1;
@@ -11,4 +18,16 @@ void merge(int* nums1, int m, int* nums2, int n) {
 
     while(j >= 0)
         nums1[end--] = nums2[j--];
+}
+
+int main() {
+    int nums1[6] = {1, 2, 3, 0, 0, 0};
+    int nums2[3] = {2, 5, 6};
+    int m = 3, n = 3;
+    merge(nums1, m, nums2, n);
+    for (int i = 0; i < m + n; i++) {
+        printf("%d ", nums1[i]);
+    }
+    printf("\n");
+    return 0;
 }
