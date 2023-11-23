ca7c7e398efc78c0134a78bc457f88376cd86b52
mathslimin <mathslimin@qq.com>
2023-11-23 22:00:26 +0800

    Update rotate-array.c

--- array/rotate-array.c
+++ array/rotate-array.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 void reverse(int *nums, int start, int end)
 {
     int tmp;
@@ -16,4 +23,17 @@ void rotate(int* nums, int numsSize, int k) {
     reverse(nums,0,numsSize - k - 1);
     reverse(nums,numsSize - k,numsSize -1);
     reverse(nums,0,numsSize-1);
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 4, 5, 6, 7};
+    int numsSize = sizeof(nums) / sizeof(int);
+    int k = 3;
+    rotate(nums, numsSize, k);
+    printf("After rotating %d steps, the array becomes: ", k);
+    for (int i = 0; i < numsSize; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+    return 0;
 }
