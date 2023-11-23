8cac6ba23afac2cdc860dcd9c5eb5e6dd0099577
mathslimin <mathslimin@qq.com>
2023-11-23 22:05:33 +0800

    Update spiral-matrix.c

--- array/spiral-matrix.c
+++ array/spiral-matrix.c
@@ -1,6 +1,10 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 int* spiralOrder(int** matrix, int matrixRowSize, int matrixColSize) {
     int size = matrixRowSize * matrixColSize;
     int i,j,count;
@@ -60,4 +64,32 @@ int* spiralOrder(int** matrix, int matrixRowSize, int matrixColSize) {
     }
 
     return ret;
+}
+
+int main(){
+    int matrixSize = 3;
+    int matrixColSize[] = {3, 4, 5};
+    int** matrix = (int**)malloc(sizeof(int*) * matrixSize);
+    for(int i = 0; i < matrixSize; i++){
+        matrix[i] = (int*)malloc(sizeof(int) * matrixColSize[i]);
+        for(int j = 0; j < matrixColSize[i]; j++){
+            matrix[i][j] = i * matrixColSize[i] + j + 1;
+        }
+    }
+    int returnSize;
+    int* res = spiralOrder(matrix, matrixSize, matrixColSize, &returnSize);
+    printf("[");
+    for(int i = 0; i < returnSize; i++){
+        printf("%d", res[i]);
+        if(i != returnSize - 1){
+            printf(", ");
+        }
+    }
+    printf("]");
+    for(int i = 0; i < matrixSize; i++){
+        free(matrix[i]);
+    }
+    free(matrix);
+    free(res);
+    return 0;
 }
