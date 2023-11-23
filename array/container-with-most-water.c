3127465f315bc75ce608e9a5ecc49fe58a7c8e41
mathslimin <mathslimin@qq.com>
2023-11-23 21:43:31 +0800

    Update container-with-most-water.c

--- array/container-with-most-water.c
+++ array/container-with-most-water.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 int maxArea(int* height, int heightSize) {
     int ret = 0;
     int start,end;
@@ -18,4 +25,12 @@ int maxArea(int* height, int heightSize) {
     }
 
     return ret;
+}
+
+int main() {
+    int height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    int heightSize = sizeof(height) / sizeof(height[0]);
+    int result = maxArea(height, heightSize);
+    printf("The maximum area is: %d", result);
+    return 0;
 }
