4811e1d107e14c5bc7c436a41a57b33954c44bb8
mathslimin <mathslimin@qq.com>
2023-11-23 21:59:28 +0800

    Update remove-duplicates-from-sorted-array-ii.c

--- array/remove-duplicates-from-sorted-array-ii.c
+++ array/remove-duplicates-from-sorted-array-ii.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 int removeDuplicates(int* nums, int numsSize) {
     int count,i;
 
@@ -11,4 +18,17 @@ int removeDuplicates(int* nums, int numsSize) {
     }
 
     return count;
+}
+
+int main() {
+    int nums[] = {1,1,1,2,2,3};
+    int numsSize = sizeof(nums) / sizeof(int);
+    int newNumsSize = removeDuplicates(nums, numsSize);
+    printf("new array: ");
+    for (int i = 0; i < newNumsSize; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
