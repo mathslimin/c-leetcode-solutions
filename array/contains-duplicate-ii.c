d9f50de0aad3a4c04185b8b48dba5dd987f1132e
mathslimin <mathslimin@qq.com>
2023-11-23 21:43:54 +0800

    Update contains-duplicate-ii.c

--- array/contains-duplicate-ii.c
+++ array/contains-duplicate-ii.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
     int i,j;
 
@@ -8,4 +15,17 @@ bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
     }
 
     return false;
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 1, 2, 3};
+    int numsSize = 6;
+    int k = 2;
+    bool result = containsNearbyDuplicate(nums, numsSize, k);
+    if (result) {
+        printf("The array contains nearby duplicates within %d indices.", k);
+    } else {
+        printf("The array does not contain nearby duplicates within %d indices.", k);
+    }
+    return 0;
 }
