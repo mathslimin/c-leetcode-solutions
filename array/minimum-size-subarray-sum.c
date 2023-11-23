c0fa3d028530bb6fce52b1a496174dd25a0a4fc4
mathslimin <mathslimin@qq.com>
2023-11-23 21:53:09 +0800

    Update minimum-size-subarray-sum.c

--- array/minimum-size-subarray-sum.c
+++ array/minimum-size-subarray-sum.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 int minSubArrayLen(int s, int* nums, int numsSize) {
     int i,j;
     int sum;
@@ -23,4 +30,14 @@ int minSubArrayLen(int s, int* nums, int numsSize) {
     }
 
     return ret == numsSize + 1 ? 0 : ret;
+}
+
+int main() {
+    int s = 7;
+    int nums[] = {2,3,1,2,4,3};
+    int numsSize = 6;
+
+    printf("%d", minSubArrayLen(s, nums, numsSize));
+
+    return 0;
 }
