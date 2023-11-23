b2c17d1d29e8cb8ac3434f91a8f28fd4c4fc6dfe
mathslimin <mathslimin@qq.com>
2023-11-23 21:50:00 +0800

    Update majority-element.c

--- array/majority-element.c
+++ array/majority-element.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 int majorityElement(int* nums, int numsSize) {
     int count;
     int ret;
@@ -14,4 +21,13 @@ int majorityElement(int* nums, int numsSize) {
     }
 
     return ret;
+}
+
+int main() {
+    int nums[] = {1, 1, 1, 2, 2, 3};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int returnSize = 0;
+    int res = majorityElement(nums, numsSize);
+    printf("res: %d ", res);
+    return 0;
 }
