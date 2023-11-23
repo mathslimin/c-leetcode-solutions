b4cd31e6cb908d3a36feb2603ef0c03e1f5e5dda
mathslimin <mathslimin@qq.com>
2023-11-23 21:45:53 +0800

    Update find-peak-element.c

--- array/find-peak-element.c
+++ array/find-peak-element.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 int findPeakElement(int* nums, int numsSize) {
     int i;
     for(i = 1; i < numsSize; ++i)
@@ -5,4 +12,12 @@ int findPeakElement(int* nums, int numsSize) {
             return i-1;
 
     return numsSize - 1;
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 1};
+    int numsSize = sizeof(nums) / sizeof(int);
+    int peak = findPeakElement(nums, numsSize);
+    printf("The peak element is %d\n", nums[peak]);
+    return 0;
 }
