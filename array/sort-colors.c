e9f949cefbbe1e5b8197383666c8ab9b10dedf5a
mathslimin <mathslimin@qq.com>
2023-11-23 22:04:08 +0800

    Update sort-colors.c

--- array/sort-colors.c
+++ array/sort-colors.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 void swap(int *a, int *b)
 {
     int tmp = *a;
@@ -23,4 +30,14 @@ void sortColors(int* nums, int numsSize) {
             right--;
         }
     }
+}
+
+int main(){
+    int nums[] = {2,0,2,1,1,0};
+    int numsSize = 6;
+    sortColors(nums, numsSize);
+    for(int i=0; i<numsSize; i++){
+        printf("%d ", nums[i]);
+    }
+    return 0;
 }
