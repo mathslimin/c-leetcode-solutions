233376cf2b21cba6ef784a15c057d59c37d9460b
mathslimin <mathslimin@qq.com>
2023-11-23 22:00:06 +0800

    Update remove-element.c

--- array/remove-element.c
+++ array/remove-element.c
@@ -1,3 +1,10 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
 int removeElement(int* nums, int numsSize, int val) {
     int count,i;
 
@@ -8,4 +15,18 @@ int removeElement(int* nums, int numsSize, int val) {
     }
 
     return count;
+}
+
+int main() {
+    int nums[] = {3,2,2,3};
+    int val = 3;
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int newLen = removeElement(nums, numsSize, val);
+    printf("New array length: %d", newLen);
+    printf("New array: ");
+    for (int i = 0; i < newLen; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+    return 0;
 }
